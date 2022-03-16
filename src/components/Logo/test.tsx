492326a463c99d8e3d2efdd252cab4548c79952a
Luiz Henrique <luizhjramos@outlook.com>
2022-03-16 19:04:53 -0300

    test: should render a normal logo when size is default case

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -28,4 +28,11 @@ describe("<Logo />", () => {
       width: "20rem"
     })
   })
+
+  it("should render a normal logo when size is default", () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: "11rem"
+    })
+  })
 })
