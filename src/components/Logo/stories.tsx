b0442a8b7b8456eb1fc9d26ce62839ba5d1f9d39
Luiz Henrique <luizhjramos@outlook.com>
2022-03-16 11:31:39 -0300

    feat: Logo storybook args

--- src/components/Logo/stories.tsx
+++ src/components/Logo/stories.tsx
@@ -4,9 +4,12 @@ import { Story, Meta } from "@storybook/react/types-6-0"
 // Components
 import Logo from "."
 
+// Types
+import { LogoProps } from "."
+
 export default {
   title: "Logo",
   component: Logo
 } as Meta
 
-export const Default: Story = () => <Logo />
+export const Default: Story<LogoProps> = (args) => <Logo {...args} />
