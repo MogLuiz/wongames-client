a30a9c64e8b4845bd6e847a1953ce2182868340c
Luiz Henrique <luizhjramos@outlook.com>
2022-03-16 12:42:50 -0300

    feat: helper for testing with theme provider

---
+++ src/utils/tests/helpers.tsx
@@ -0,0 +1,12 @@
+// Packages
+import React from "react"
+import { render, RenderResult } from "@testing-library/react"
+
+// Theme
+import theme from "styles/theme"
+
+// Theme Provider
+import { ThemeProvider } from "styled-components"
+
+export const renderWithTheme = (children: React.ReactNode): RenderResult =>
+  render(<ThemeProvider theme={theme}>{children}</ThemeProvider>)
