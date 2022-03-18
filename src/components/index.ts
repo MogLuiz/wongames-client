0404bcbe2b910c22fb8073e0889947034f5a003b
Luiz Henrique <luizhjramos@outlook.com>
2022-03-18 16:55:22 -0300

    feat: new export components architecture

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Heading } from "./Heading"
+export { default as Logo } from "./Logo"
+export { default as Main } from "./Main"
+export { default as MediaMatch } from "./MediaMatch"
