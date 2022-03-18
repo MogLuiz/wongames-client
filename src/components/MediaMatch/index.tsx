8a33f2943901c798b0343268c14b0443a72c992d
Luiz Henrique <luizhjramos@outlook.com>
2022-03-18 08:18:42 -0300

    feat: display none when not receiving props

--- src/components/MediaMatch/index.tsx
+++ src/components/MediaMatch/index.tsx
@@ -1,4 +1,18 @@
 // Packages
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
-export default styled.div``
+// Types
+import media, { DefaultBreakpoints } from "styled-media-query"
+
+type breakpoint = keyof DefaultBreakpoints
+
+export type MediaMatchProps = {
+  lessThan?: breakpoint
+  greaterThan?: breakpoint
+}
+
+export default styled.div<MediaMatchProps>`
+  ${({ lessThan, greaterThan }) => css`
+    display: none;
+  `}
+`
