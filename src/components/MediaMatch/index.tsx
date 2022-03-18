32547de8d07fd10e726062b9c5a4a8265f0c6304
Luiz Henrique <luizhjramos@outlook.com>
2022-03-18 20:25:57 -0300

    feat: MediaMatch modifiers

--- src/components/MediaMatch/index.tsx
+++ src/components/MediaMatch/index.tsx
@@ -11,8 +11,21 @@ export type MediaMatchProps = {
   greaterThan?: breakpoint
 }
 
+const mediaMatchModifiers = {
+  lessThan: (size: breakpoint) => css`
+    ${media.lessThan(size)` display: block`}
+  `,
+
+  greaterThan: (size: breakpoint) => css`
+    ${media.greaterThan(size)`display: block`}
+  `
+}
+
 export default styled.div<MediaMatchProps>`
   ${({ lessThan, greaterThan }) => css`
     display: none;
+
+    ${!!lessThan && mediaMatchModifiers.lessThan(lessThan)}
+    ${!!greaterThan && mediaMatchModifiers.greaterThan(greaterThan)}
   `}
 `
