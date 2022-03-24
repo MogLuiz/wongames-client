0e83c8d6257e8687fbc20e670d96690905fafb6b
Luiz Henrique <luizhjramos@outlook.com>
2022-03-24 12:40:36 -0300

    test: should render the small size

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -17,4 +17,13 @@ describe("<Button />", () => {
       "font-size": "1.4rem"
     })
   })
+
+  it("should render the small size", () => {
+    renderWithTheme(<Button size="small">Buy now</Button>)
+
+    expect(screen.getByRole("button", { name: /Buy now/i })).toHaveStyle({
+      height: "3rem",
+      "font-size": "1.2rem"
+    })
+  })
 })
