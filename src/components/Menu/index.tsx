dbe0b8662626f675f1c48022d43e730f210eaf17
Luiz Henrique <luizhjramos@outlook.com>
2022-03-26 15:13:09 -0300

    feat: Menu assets

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,14 @@
 // Packages
 import React from "react"
 
+// Assets
+import { ShoppingCart as ShoppingCartIcon } from "@styled-icons/material-outlined/ShoppingCart"
+import { Menu2 as MenuIcon } from "@styled-icons/remix-fill/Menu2"
+import { Search as SearchIcon } from "@styled-icons/material-outlined/Search"
+
+// Components
+import { Logo } from ".."
+
 // Styles
 import * as S from "./styles"
 
