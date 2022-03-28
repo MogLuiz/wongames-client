561ee443d6c03a9d249f4fcdef2142d7c6632c98
Luiz Henrique <luizhjramos@outlook.com>
2022-03-28 13:37:35 -0300

    TDD test: simulating click and testing if the menu was opened

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,5 +1,5 @@
 // Packages
-import { screen } from "@testing-library/react"
+import { fireEvent, screen } from "@testing-library/react"
 import { renderWithTheme } from "utils/tests/helpers"
 
 // Components
@@ -24,7 +24,9 @@ describe("<Menu />", () => {
     expect(fullMenuElement.getAttribute("aria-hidden")).toBe("true")
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
     // Clicar no botão de abrir o menu e verificar se ele abriu
-
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute("aria-hidden")).toBe("false")
+    // expect(fullMenuElement).toHaveStyle({ opacity: 1 })
     // Clicar no botão de fechar o menu e verificar se ele fechou
   })
 })
