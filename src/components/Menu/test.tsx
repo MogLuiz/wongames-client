5710b451e06719fc65b1c8870ab066326daa637e
Luiz Henrique <luizhjramos@outlook.com>
2022-03-27 23:09:57 -0300

    TDD test: select full menu element

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -14,4 +14,16 @@ describe("<Menu />", () => {
     expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/Won Games/i)).toBeInTheDocument()
   })
+
+  it("should handle the open/close mobile menu", () => {
+    renderWithTheme(<Menu />)
+
+    // Selecionar o nosso MenuFull
+    const fullMenuElement = screen.getByRole("navigation", { hidden: true })
+    // Verificar se o menu tá escondido
+
+    // Clicar no botão de abrir o menu e verificar se ele abriu
+
+    // Clicar no botão de fechar o menu e verificar se ele fechou
+  })
 })
