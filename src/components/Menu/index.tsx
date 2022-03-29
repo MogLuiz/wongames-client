28edda1ec6eb42cf295c1af2d18ace64c592d965
Luiz Henrique <luizhjramos@outlook.com>
2022-03-29 19:55:51 -0300

    feat: added new menu options

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -41,17 +41,22 @@ const Menu: React.FC = () => {
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
         <S.MenuNav>
-          <S.MenuLink href="#">Home</S.MenuLink>
-          <S.MenuLink href="#">Explore</S.MenuLink>
+          <S.MenuLink href="#">Início</S.MenuLink>
+          <S.MenuLink href="#">Explorar</S.MenuLink>
+
+          <>
+            <S.MenuLink href="#">Minha conta</S.MenuLink>
+            <S.MenuLink href="#">Lista de desejos</S.MenuLink>
+          </>
         </S.MenuNav>
 
         <S.RegisterBox>
           <Button fullWidth size="large">
-            Log in now
+            Entrar
           </Button>
-          <span>or</span>
+          <span>ou</span>
           <S.CreateAccount href="#" title="Sign In">
-            Sign Up
+            Crie sua conta
           </S.CreateAccount>
         </S.RegisterBox>
       </S.MenuFull>
