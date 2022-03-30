0b670d75f7f0a47fbf92ff9ebf2b64d2335f689c
Luiz Henrique <luizhjramos@outlook.com>
2022-03-30 15:35:25 -0300

    feat: Structure Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,9 @@
 // Packages
 import React from "react"
+import Link from "next/link"
+
+// Component
+import { Heading, Logo } from ".."
 
 // Styles
 import * as S from "./styles"
@@ -9,7 +13,82 @@ const Footer: React.FC = () => (
   // Render
   // -------------------------------------------------
   <S.Wrapper>
-    <h1>Footer</h1>
+    <Logo color="black" />
+    <S.Content>
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          CONTATO
+        </Heading>
+
+        <a href="malito:sac@wongames.com">suporte@wongames.gg</a>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          NOS ACOMPANHE
+        </Heading>
+
+        <nav aria-labelledby="social media">
+          <a
+            href="https://www.instagram.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://www.twitter.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            href="https://www.youtube.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Youtube
+          </a>
+          <a
+            href="https://www.facebook.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Facebook
+          </a>
+        </nav>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" lineColor="secondary" lineBottom size="small">
+          Links
+        </Heading>
+
+        <nav aria-labelledby="footer resources">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/games">
+            <a>Store</a>
+          </Link>
+          <Link href="/search">
+            <a>Buscar</a>
+          </Link>
+        </nav>
+      </S.Column>
+
+      <S.Column aria-labelledby="footer-contact">
+        <Heading color="black" lineColor="secondary" lineBottom size="small">
+          Localização
+        </Heading>
+        <span>Lorem ipsum dolor sit.</span>
+        <span>Lorem Ipsum</span>
+        <span>Lorem, ipsum dolor.</span>
+      </S.Column>
+    </S.Content>
+
+    <S.Copyright>Won Games 2022 © Todos os Direitos Reservados.</S.Copyright>
   </S.Wrapper>
 )
 
