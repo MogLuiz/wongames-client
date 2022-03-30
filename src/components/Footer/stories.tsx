49333e0846b98d4d3641944f4bcc14f172b4ae56
Luiz Henrique <luizhjramos@outlook.com>
2022-03-30 14:13:43 -0300

    feat: initialize the Footer component

---
+++ src/components/Footer/stories.tsx
@@ -0,0 +1,12 @@
+// Packages
+import { Story, Meta } from "@storybook/react/types-6-0"
+
+// Components
+import Footer from "."
+
+export default {
+  title: "Footer",
+  component: Footer
+} as Meta
+
+export const Default: Story = () => <Footer />
---
+++ src/components/Footer/test.tsx
@@ -0,0 +1,15 @@
+// Packages
+import { render, screen } from "@testing-library/react"
+
+// Components
+import Footer from "."
+
+describe("<Footer />", () => {
+  it("should render the heading", () => {
+    const { container } = render(<Footer />)
+
+    expect(screen.getByRole("heading", { name: /Footer/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
