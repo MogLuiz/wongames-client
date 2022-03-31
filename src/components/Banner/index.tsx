e863dc8d3bda87de9b1595d808e921dfe506ca74
Luiz Henrique <luizhjramos@outlook.com>
2022-03-31 13:15:43 -0300

    feat: Banner component props

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -4,7 +4,21 @@ import React from "react"
 // Styles
 import * as S from "./styles"
 
-const Banner: React.FC = () => (
+export type BannerProps = {
+  img: string
+  title: string
+  subtitle: string
+  buttonLabel: string
+  buttonLink: string
+}
+
+const Banner: React.FC<BannerProps> = ({
+  buttonLabel,
+  buttonLink,
+  img,
+  subtitle,
+  title
+}) => (
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
