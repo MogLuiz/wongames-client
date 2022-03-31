dce9d7ac77f19c915939d3ecf9499d8e4a9b71aa
Luiz Henrique <luizhjramos@outlook.com>
2022-03-31 10:59:04 -0300

    feat: asLink storie component version

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -33,3 +33,12 @@ withIcon.args = {
   children: "Buy now",
   icon: <AddShoppingCart />
 }
+
+export const asLink: Story<ButtonProps> = (args) => <Button {...args} />
+
+asLink.args = {
+  size: "large",
+  children: "Buy now",
+  as: "a",
+  href: "/link"
+}
