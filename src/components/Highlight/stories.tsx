6c209a0b890a267b5d97d7f8f16457c2ed85d1a6
Luiz Henrique <luizhjramos@outlook.com>
2022-04-04 22:40:57 -0300

    feat: initialize the Highlight component

---
+++ src/components/Highlight/stories.tsx
@@ -0,0 +1,12 @@
+// Packages
+import { Story, Meta } from "@storybook/react/types-6-0"
+
+// Components
+import Highlight from "."
+
+export default {
+  title: "Highlight",
+  component: Highlight
+} as Meta
+
+export const Default: Story = () => <Highlight />
