ef56235d10a47dbe3a3226e2b62bfd107ccbe948
Luiz Henrique <luizhjramos@outlook.com>
2022-04-04 23:47:30 -0300

    test: should render heading and button

--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -1,14 +1,28 @@
 // Packages
 import { render, screen } from "@testing-library/react"
 
+// Utils
+import { renderWithTheme } from "utils/tests/helpers"
+
 // Components
 import Highlight from "."
 
-describe("<Highlight />", () => {
-  it("should render the heading", () => {
-    const { container } = render(<Highlight />)
+const props = {
+  title: "Heading 1",
+  subtitle: "Heading 2"
+}
 
-    expect(screen.getByRole("heading", { name: /Highlight/i })).toBeInTheDocument()
+describe("<Highlight />", () => {
+  it("should render heading and button", () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+
+    expect(
+      screen.getByRole("heading", { name: /heading 1/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole("heading", { name: /heading 2/i })
+    ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
   })
