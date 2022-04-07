9acb04b3dd143bee80cd832531d88dfc922ee61d
Luiz Henrique <luizhjramos@outlook.com>
2022-04-07 13:03:21 -0300

    test: should render the heading

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -1,15 +1,36 @@
 // Packages
-import { render, screen } from "@testing-library/react"
+import { screen } from "@testing-library/react"
+
+// Utils
+import { renderWithTheme } from "utils/tests/helpers"
 
 // Components
 import GameCard from "."
 
+const props = {
+  title: "Population Zero",
+  developer: "Rockstar Games",
+  img: "https://source.unsplash.com/user/willianjusten/300x140",
+  price: "R$ 235,00"
+}
+
 describe("<GameCard />", () => {
   it("should render the heading", () => {
-    const { container } = render(<GameCard />)
+    renderWithTheme(<GameCard {...props} />)
 
-    expect(screen.getByRole("heading", { name: /GameCard/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole("heading", { name: props.title })
+    ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole("heading", { name: props.developer })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole("img", { name: props.title })).toHaveAttribute(
+      "src",
+      props.img
+    )
+
+    expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument()
   })
 })
