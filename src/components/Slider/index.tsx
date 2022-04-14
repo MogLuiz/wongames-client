d8a742830a05f58914f7a64f3c0d5a13d23ed1bd
Luiz Henrique <luizhjramos@outlook.com>
2022-04-14 20:37:25 -0300

    feat: initialize Slider component

---
+++ src/components/Slider/index.tsx
@@ -0,0 +1,16 @@
+// Packages
+import React from "react"
+
+// Styles
+import * as S from "./styles"
+
+const Slider: React.FC = () => (
+  // -------------------------------------------------
+  // Render
+  // -------------------------------------------------
+  <S.Wrapper>
+    <h1>Slider</h1>
+  </S.Wrapper>
+)
+
+export default Slider
---
+++ src/components/Slider/stories.tsx
@@ -0,0 +1,12 @@
+// Packages
+import { Story, Meta } from "@storybook/react/types-6-0"
+
+// Components
+import Slider from "."
+
+export default {
+  title: "Slider",
+  component: Slider
+} as Meta
+
+export const Default: Story = () => <Slider />
