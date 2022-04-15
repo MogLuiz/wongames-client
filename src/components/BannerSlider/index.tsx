677b8dfd864b2c4a7a25cf15eeaac03d4db478e1
Luiz Henrique <luizhjramos@outlook.com>
2022-04-15 16:51:02 -0300

    feat: BannerSlider structure

--- src/components/BannerSlider/index.tsx
+++ src/components/BannerSlider/index.tsx
@@ -1,15 +1,29 @@
 // Packages
 import React from "react"
 
+// Components
+import Slider from "../Slider"
+import Banner, { BannerProps } from "components/Banner"
+
 // Styles
 import * as S from "./styles"
 
-const BannerSlider: React.FC = () => (
+type BannerSliderProps = {
+  items: BannerProps[]
+}
+
+const settings = {}
+
+const BannerSlider: React.FC<BannerSliderProps> = ({ items }) => (
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
   <S.Wrapper>
-    <h1>BannerSlider</h1>
+    <Slider settings={settings}>
+      {items.map((item: BannerProps) => (
+        <Banner key={item.title} {...item} />
+      ))}
+    </Slider>
   </S.Wrapper>
 )
 
