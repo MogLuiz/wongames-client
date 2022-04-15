f75635da37b1684e8cb147b0b896ccae22c42003
Luiz Henrique <luizhjramos@outlook.com>
2022-04-15 18:22:26 -0300

    test: should render vertical slider

--- src/components/BannerSlider/test.tsx
+++ src/components/BannerSlider/test.tsx
@@ -1,15 +1,37 @@
 // Packages
-import { render, screen } from "@testing-library/react"
+import { screen } from "@testing-library/react"
+
+// Mock
+import "match-media-mock"
+
+// Utils
+import { renderWithTheme } from "utils/tests/helpers"
 
 // Components
 import BannerSlider from "."
 
+const items = [
+  {
+    img: "https://source.unsplash.com/user/willianjusten/1042x580",
+    title: "Defy death 1",
+    subtitle: "<p>Play the new <strong>CrashLands</strong> season",
+    buttonLabel: "Buy now",
+    buttonLink: "/games/defy-death",
+    ribbon: "Bestselling"
+  },
+  {
+    img: "https://source.unsplash.com/user/willianjusten/1042x582",
+    title: "Defy death 2",
+    subtitle: "<p>Play the new <strong>CrashLands</strong> season",
+    buttonLabel: "Buy now",
+    buttonLink: "/games/defy-death"
+  }
+]
+
 describe("<BannerSlider />", () => {
-  it("should render the heading", () => {
-    const { container } = render(<BannerSlider />)
+  it("should render vertical slider", () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
 
-    expect(screen.getByRole("heading", { name: /BannerSlider/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(container.querySelector(".slick-vertical")).toBeInTheDocument()
   })
 })
