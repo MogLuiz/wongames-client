7168b0bf1a15815a986b3a79c46e9eba455f2dbe
Luiz Henrique <luizhjramos@outlook.com>
2022-04-14 22:24:32 -0300

    feat: Horizontal Slider storybook

--- src/components/Slider/stories.tsx
+++ src/components/Slider/stories.tsx
@@ -4,9 +4,41 @@ import { Story, Meta } from "@storybook/react/types-6-0"
 // Components
 import Slider from "."
 
+// Types
+import { Settings } from "react-slick"
+
+// Styles
+import styled from "styled-components"
+
 export default {
   title: "Slider",
   component: Slider
 } as Meta
 
-export const Default: Story = () => <Slider />
+const settings: Settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+}
+
+const Slide = styled.div`
+  background: gray;
+  width: 30rem;
+  padding: 10rem 0;
+  border: 0.1rem solid red;
+  color: white;
+  text-align: center;
+`
+
+export const Horizontal: Story = () => (
+  <Slider settings={settings}>
+    <Slide>1</Slide>
+    <Slide>2</Slide>
+    <Slide>3</Slide>
+    <Slide>4</Slide>
+    <Slide>5</Slide>
+    <Slide>6</Slide>
+  </Slider>
+)
