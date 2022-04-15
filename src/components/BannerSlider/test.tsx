a5e2b088a3eb22da3bd847f29bfb3ef269199cb5
Luiz Henrique <luizhjramos@outlook.com>
2022-04-15 18:34:49 -0300

    test: should render vertical slider

--- src/components/BannerSlider/test.tsx
+++ src/components/BannerSlider/test.tsx
@@ -34,4 +34,19 @@ describe("<BannerSlider />", () => {
 
     expect(container.querySelector(".slick-vertical")).toBeInTheDocument()
   })
+
+  it("should render with 1 active item", () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+
+    expect(container.querySelectorAll(".slick-slide")).toHaveLength(2)
+    expect(container.querySelectorAll("li.slick-active")).toHaveLength(1)
+
+    expect(
+      screen.getByRole("heading", { name: /defy death 1/i, hidden: false })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole("heading", { name: /defy death 2/i, hidden: true })
+    ).toBeInTheDocument()
+  })
 })
