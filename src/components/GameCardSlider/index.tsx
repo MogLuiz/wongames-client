34b12543b6aa5363fec8139044d79e5269ae0561
Luiz Henrique <luizhjramos@outlook.com>
2022-04-18 20:14:05 -0300

    feat: GameCardSlider structure

--- src/components/GameCardSlider/index.tsx
+++ src/components/GameCardSlider/index.tsx
@@ -1,15 +1,33 @@
 // Packages
 import React from "react"
 
+// Components
+import GameCard, { GameCardProps } from "components/GameCard"
+import Slider, { SliderSettings } from "components/Slider"
+
 // Styles
 import * as S from "./styles"
 
-const GameCardSlider: React.FC = () => (
+type GameCardSliderProps = {
+  items: GameCardProps[]
+  color?: "white" | "black"
+}
+
+const settings: SliderSettings = {}
+
+const GameCardSlider: React.FC<GameCardSliderProps> = ({
+  items,
+  color = "black"
+}) => (
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
-  <S.Wrapper>
-    <h1>GameCardSlider</h1>
+  <S.Wrapper color={color}>
+    <Slider settings={settings}>
+      {items.map((item, index) => (
+        <GameCard key={index} {...item} />
+      ))}
+    </Slider>
   </S.Wrapper>
 )
 
