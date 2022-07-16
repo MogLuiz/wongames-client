cd0fc90c0b0670986e4dc970f904a915e317886e
Luiz Henrique <luizhjramos@outlook.com>
2022-07-15 22:09:28 -0300

    test: factory setup test to abstract test utils

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -7,9 +7,26 @@ import { renderWithTheme } from "utils/tests/helpers"
 // Components
 import Home from "."
 
+const factorySetupTest = () => {
+  const utils = renderWithTheme(<Home />)
+
+  const releaseSection = screen.getByRole("heading", { name: /New Releases/i })
+  const popularSection = screen.getByRole("heading", { name: /Most Popular/i })
+  const upcommingSection = screen.getByRole("heading", { name: /Upcomming/i })
+  const freeSection = screen.getByRole("heading", { name: /Free Games/i })
+
+  return {
+    ...utils,
+    freeSection,
+    releaseSection,
+    popularSection,
+    upcommingSection
+  }
+}
+
 describe("<Home />", () => {
   it("should render the Menu and Footer", () => {
-    renderWithTheme(<Home />)
+    factorySetupTest()
 
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
     expect(
@@ -18,19 +35,12 @@ describe("<Home />", () => {
   })
 
   it("should render the sections", () => {
-    renderWithTheme(<Home />)
+    const { freeSection, popularSection, releaseSection, upcommingSection } =
+      factorySetupTest()
 
-    expect(
-      screen.getByRole("heading", { name: /New Releases/i })
-    ).toBeInTheDocument()
-    expect(
-      screen.getByRole("heading", { name: /Most Popular/i })
-    ).toBeInTheDocument()
-    expect(
-      screen.getByRole("heading", { name: /Upcomming/i })
-    ).toBeInTheDocument()
-    expect(
-      screen.getByRole("heading", { name: /Free Games/i })
-    ).toBeInTheDocument()
+    expect(freeSection).toBeInTheDocument()
+    expect(popularSection).toBeInTheDocument()
+    expect(releaseSection).toBeInTheDocument()
+    expect(upcommingSection).toBeInTheDocument()
   })
 })
