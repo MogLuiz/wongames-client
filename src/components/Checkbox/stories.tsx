dd00877d22b29a98c8e47d32710500798812595b
Luiz Henrique <luizhjramos@outlook.com>
2022-07-18 07:16:07 -0300

    test: factory setup test to abstract test utils

--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -2,11 +2,11 @@
 import { Story, Meta } from "@storybook/react/types-6-0"
 
 // Components
-import Checkbox from "."
+import Checkbox, { CheckboxProps } from "."
 
 export default {
   title: "Checkbox",
   component: Checkbox
 } as Meta
 
-export const Default: Story = () => <Checkbox />
+export const Default: Story<CheckboxProps> = (args) => <Checkbox {...args} />
--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -5,17 +5,37 @@ import { renderWithTheme } from "utils/tests/helpers"
 // Components
 import Checkbox from "."
 
+const factorySetupTest = () => {
+  const utils = renderWithTheme(
+    <Checkbox label="checkbox label" labelFor="check" />
+  )
+
+  const InputElementByCheckboxRole = screen.getByRole("checkbox")
+  // this getting the input because the html attribute for associates the label with ID with the same value
+  const InputElementByCheckboxLabelText =
+    screen.getByLabelText(/checkbox label/i)
+  const checkBoxElementByText = screen.getByText(/checkbox label/i)
+
+  return {
+    InputElementByCheckboxRole,
+    InputElementByCheckboxLabelText,
+    checkBoxElementByText,
+    ...utils
+  }
+}
+
 describe("<Checkbox />", () => {
   it("should render with label", () => {
-    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
+    const { InputElementByCheckboxRole, InputElementByCheckboxLabelText } =
+      factorySetupTest()
 
-    expect(screen.getByRole("checkbox")).toBeInTheDocument()
-    expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
+    expect(InputElementByCheckboxRole).toBeInTheDocument()
+    expect(InputElementByCheckboxLabelText).toBeInTheDocument()
   })
 
   it("should make the correct association of the htmlFor(for) attribute", () => {
-    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
+    const { checkBoxElementByText } = factorySetupTest()
 
-    expect(screen.getByText(/checkbox label/i)).toHaveAttribute("for", "check")
+    expect(checkBoxElementByText).toHaveAttribute("for", "check")
   })
 })
