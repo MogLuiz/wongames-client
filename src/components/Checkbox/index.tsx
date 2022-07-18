1d2fe7c0b58bbbb25ca4a9317863049fe19aa9b5
Luiz Henrique <luizhjramos@outlook.com>
2022-07-17 23:25:25 -0300

    feat(Checkbox): initialize test, styles and stories of Checkbox component

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,16 @@
+// Packages
+import React from "react"
+
+// Styles
+import * as S from "./styles"
+
+const Checkbox: React.FC = () => (
+  // -------------------------------------------------
+  // Render
+  // -------------------------------------------------
+  <S.Wrapper>
+    <h1>Checkbox</h1>
+  </S.Wrapper>
+)
+
+export default Checkbox
---
+++ src/components/Checkbox/stories.tsx
@@ -0,0 +1,12 @@
+// Packages
+import { Story, Meta } from "@storybook/react/types-6-0"
+
+// Components
+import Checkbox from "."
+
+export default {
+  title: "Checkbox",
+  component: Checkbox
+} as Meta
+
+export const Default: Story = () => <Checkbox />
