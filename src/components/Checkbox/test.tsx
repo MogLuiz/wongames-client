19bd4dc6fdf7edfebefbb64b41990e92d7dcce2d
Luiz Henrique <luizhjramos@outlook.com>
2022-07-17 23:35:56 -0300

    test(Checkbox): render with label

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -1,11 +1,15 @@
 // Packages
-import { render, screen } from "@testing-library/react"
+import { screen } from "@testing-library/react"
+import { renderWithTheme } from "utils/tests/helpers"
 
 // Components
 import Checkbox from "."
 
 describe("<Checkbox />", () => {
-  it("should render the heading", () => {
-    const { container } = render(<Checkbox />)
+  it("should render with label", () => {
+    renderWithTheme(<Checkbox label="checkbox label" />)
+
+    expect(screen.getByRole("checkbox")).toBeInTheDocument()
+    expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
   })
 })
