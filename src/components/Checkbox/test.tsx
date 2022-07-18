ccbfda3b0496761f4216d8eb11a22a5237baf698
Luiz Henrique <luizhjramos@outlook.com>
2022-07-18 06:56:55 -0300

    test: ensuring that the htmlFor attribute is being associated correctly

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -7,9 +7,15 @@ import Checkbox from "."
 
 describe("<Checkbox />", () => {
   it("should render with label", () => {
-    renderWithTheme(<Checkbox label="checkbox label" />)
+    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
 
     expect(screen.getByRole("checkbox")).toBeInTheDocument()
     expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
+  })
+
+  it("should make the correct association of the htmlFor(for) attribute", () => {
+    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
+
+    expect(screen.getByText(/checkbox label/i)).toHaveAttribute("for", "check")
   })
 })
