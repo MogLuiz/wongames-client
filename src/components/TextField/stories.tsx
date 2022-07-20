45987788d40751056b92f9d7382a6c276dd6c0db
Luiz Henrique <luizhjramos@outlook.com>
2022-07-20 13:56:30 -0300

    feat: initialize TextField storybook, test, style and component

---
+++ src/components/TextField/stories.tsx
@@ -0,0 +1,12 @@
+// Packages
+import { Story, Meta } from "@storybook/react/types-6-0"
+
+// Components
+import TextField from "."
+
+export default {
+  title: "TextField",
+  component: TextField
+} as Meta
+
+export const Default: Story = () => <TextField />
---
+++ src/components/TextField/test.tsx
@@ -0,0 +1,15 @@
+// Packages
+import { render, screen } from "@testing-library/react"
+
+// Components
+import TextField from "."
+
+describe("<TextField />", () => {
+  it("should render the heading", () => {
+    const { container } = render(<TextField />)
+
+    expect(screen.getByRole("heading", { name: /TextField/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
