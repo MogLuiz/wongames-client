ddcc04be7471abcee688fb370ff9220e3d31bf9b
Luiz Henrique <luizhjramos@outlook.com>
2022-07-22 17:43:47 -0300

    docs: stories by category

--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -5,7 +5,7 @@ import { Story, Meta } from "@storybook/react/types-6-0"
 import Checkbox, { CheckboxProps } from "."
 
 export default {
-  title: "Checkbox",
+  title: "Form/Checkbox",
   component: Checkbox,
   parameters: {
     layout: "fullscreen",
