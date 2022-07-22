d1ea4e149f3befa88c763033006ef25d22ddf34e
Luiz Henrique <luizhjramos@outlook.com>
2022-07-22 18:20:37 -0300

    feat: initialize template Auth test, style and component

---
+++ src/templates/Auth/test.tsx
@@ -0,0 +1,15 @@
+// Packages
+import { render, screen } from "@testing-library/react"
+
+// Components
+import Auth from "."
+
+describe("<Auth />", () => {
+  it("should render the heading", () => {
+    const { container } = render(<Auth />)
+
+    expect(screen.getByRole("heading", { name: /Auth/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
