2c12b68f0acfa8c08ca60528d51037bca9ed4c81
Luiz Henrique <luizhjramos@outlook.com>
2022-07-22 18:42:22 -0300

    feat: BannerBlock auth template structure

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -1,12 +1,24 @@
 // Packages
 import React from "react"
 
+import Logo from "components/Logo"
+import Heading from "components/Heading"
+
 // Styles
 import * as S from "./styles"
 
 const Auth = () => (
   <S.Wrapper>
-    <h1>Auth</h1>
+    <S.BannerBlock>
+      <Logo />
+
+      <Heading>Seus jogos favoritos em um só lugar</Heading>
+      <S.Subtitle>
+        A <strong>WON</strong> é a melhor e mais completa plataforma de games.
+      </S.Subtitle>
+
+      <S.Footer>Won Games 2022 © Todos os Direitos Reservados</S.Footer>
+    </S.BannerBlock>
   </S.Wrapper>
 )
 
--- src/templates/Auth/styles.ts
+++ src/templates/Auth/styles.ts
@@ -2,3 +2,9 @@
 import styled from "styled-components"
 
 export const Wrapper = styled.main``
+
+export const BannerBlock = styled.div``
+
+export const Subtitle = styled.h3``
+
+export const Footer = styled.p``
