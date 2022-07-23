6e3ce650391814824b36d4c6348b5d06c6661822
Luiz Henrique <luizhjramos@outlook.com>
2022-07-23 18:27:36 -0300

    styles: auth BannerBlock styling

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -15,14 +15,19 @@ type AuthProps = {
 const Auth = ({ children, title }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
-      <Logo />
+      <S.BannerContent>
+        <Logo />
 
-      <Heading>Seus jogos favoritos em um só lugar</Heading>
-      <S.Subtitle>
-        A <strong>WON</strong> é a melhor e mais completa plataforma de games.
-      </S.Subtitle>
+        <div>
+          <Heading>Seus jogos favoritos em um só lugar</Heading>
+          <S.Subtitle>
+            A <strong>WON</strong> é a melhor e mais completa plataforma de
+            games.
+          </S.Subtitle>
+        </div>
 
-      <S.Footer>Won Games 2022 © Todos os Direitos Reservados</S.Footer>
+        <S.Footer>Won Games 2022 © Todos os Direitos Reservados</S.Footer>
+      </S.BannerContent>
     </S.BannerBlock>
 
     <S.Content>
--- src/templates/Auth/styles.ts
+++ src/templates/Auth/styles.ts
@@ -1,12 +1,68 @@
 // Packages
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.main`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  height: 100vh;
+`
 
-export const BannerBlock = styled.div``
+export const BannerBlock = styled.div`
+  ${({ theme }) => css`
+    position: relative;
+    background-image: url(/img/auth-bg.jpg);
+    background-size: cover;
+    background-position: center center;
+    padding: ${theme.spacings.xxlarge} ${theme.spacings.xxlarge}
+      ${theme.spacings.large};
 
-export const Subtitle = styled.h3``
+    &:after {
+      content: "";
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      background: ${theme.colors.black};
+      opacity: 0.85;
+    }
+  `}
+`
 
-export const Footer = styled.p``
+export const BannerContent = styled.div`
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: 1fr;
+    justify-content: space-between;
+    height: 100%;
+    color: ${theme.colors.white};
+    position: relative;
+    z-index: ${theme.layers.base};
+  `}
+`
 
-export const Content = styled.div``
+export const Subtitle = styled.h3`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.xxlarge};
+    font-weight: ${theme.colors.primary};
+    margin-top: ${theme.spacings.xxsmall};
+
+    strong {
+      color: ${theme.colors.primary};
+    }
+  `}
+`
+
+export const Footer = styled.p`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.xsmall};
+    text-align: center;
+    align-self: end;
+  `}
+`
+
+export const Content = styled.div`
+  ${({ theme }) => css`
+    background: ${theme.colors.white};
+  `}
+`
