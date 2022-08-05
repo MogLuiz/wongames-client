40cbb72c71a09074d0cde8d1d94fe8d1d4c4cb52
Luiz Henrique <luizhjramos@outlook.com>
2022-08-05 20:50:54 -0300

    feat: FormSignIn component structure

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -1,12 +1,47 @@
 // Packages
 import React from "react"
+import Link from "next/link"
+
+// Assets
+import { Email, Lock } from "@styled-icons/material-outlined"
+
+// Components
+import TextField from "components/TextField"
 
 // Styles
 import * as S from "./styles"
+import Button from "components/Button"
 
 const FormSignIn = () => (
   <S.Wrapper>
-    <h1>FormSignIn</h1>
+    <form>
+      <TextField
+        name="email"
+        placeholder="Email"
+        type="email"
+        icon={<Email />}
+      />
+
+      <TextField
+        name="password"
+        placeholder="Password"
+        type="password"
+        icon={<Lock />}
+      />
+    </form>
+
+    <S.ForgotPassword href="#">Esqueceu sua senha?</S.ForgotPassword>
+
+    <Button size="large" fullWidth>
+      Entrar
+    </Button>
+
+    <S.FormLink>
+      Ainda não tem uma conta?
+      <Link href="/sign-up">
+        <a>Cadastre-se</a>
+      </Link>
+    </S.FormLink>
   </S.Wrapper>
 )
 
