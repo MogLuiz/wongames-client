e7491f718e85b54b4fd3e4645c9b1ef88dad6c0e
Luiz Henrique <luizhjramos@outlook.com>
2022-08-05 22:18:11 -0300

    test: ensure render the form correctly

--- src/components/FormSignIn/test.tsx
+++ src/components/FormSignIn/test.tsx
@@ -1,15 +1,16 @@
 // Packages
-import { render, screen } from "@testing-library/react"
+import { screen } from "@testing-library/react"
+import { renderWithTheme } from "utils/tests/helpers"
 
 // Components
 import FormSignIn from "."
 
 describe("<FormSignIn />", () => {
   it("should render the heading", () => {
-    const { container } = render(<FormSignIn />)
+    renderWithTheme(<FormSignIn />)
 
-    expect(screen.getByRole("heading", { name: /FormSignIn/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
+    expect(screen.getByRole("button", { name: /Entrar/i })).toBeInTheDocument()
   })
 })
