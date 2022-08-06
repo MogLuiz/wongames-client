ce57761e4fcc7ffb6dabab7c23c2c9ebb86a4b43
Luiz Henrique <luizhjramos@outlook.com>
2022-08-06 18:41:28 -0300

    test: ensure render the form correctly

--- src/components/Form/test.tsx
+++ src/components/Form/test.tsx
@@ -1,15 +1,58 @@
 // Packages
-import { render, screen } from "@testing-library/react"
+import { renderWithTheme } from "utils/tests/helpers"
 
 // Components
-import Form from "."
+import { FormWrapper, FormLink } from "."
 
 describe("<Form />", () => {
-  it("should render the heading", () => {
-    const { container } = render(<Form />)
+  it("should render the Form", () => {
+    const { container } = renderWithTheme(
+      <FormWrapper>
+        <FormLink>
+          My nice <a href="#">link</a>
+        </FormLink>
+      </FormWrapper>
+    )
 
-    expect(screen.getByRole("heading", { name: /Form/i })).toBeInTheDocument()
+    expect(container.parentElement).toMatchInlineSnapshot(`
+      .c0 {
+        font-size: 1.4rem;
+        color: #030517;
+        text-align: center;
+      }
 
-    expect(container.firstChild).toMatchSnapshot()
+      .c0 a {
+        color: #3CD3C1;
+        -webkit-text-decoration: none;
+        text-decoration: none;
+        border-bottom: 0.1rem solid #3CD3C1;
+        -webkit-transition: color,border,0.1s ease-in-out;
+        transition: color,border,0.1s ease-in-out;
+      }
+
+      .c0 a:hover {
+        color: #29b3a3;
+        border-bottom: 0.1rem solid #29b3a3;
+      }
+
+      <body>
+        <div>
+          <div
+            class=""
+          >
+            <div
+              class="c0"
+            >
+              My nice 
+              <a
+                href="#"
+              >
+                link
+              </a>
+            </div>
+          </div>
+        </div>
+      </body>
+    `)
   })
 })
