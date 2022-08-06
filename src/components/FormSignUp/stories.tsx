fe60f970698c6722d86bf8a48eca4fca5cf5c4aa
Luiz Henrique <luizhjramos@outlook.com>
2022-08-06 13:20:35 -0300

    styles: FormSignUp component styles

--- src/components/FormSignUp/stories.tsx
+++ src/components/FormSignUp/stories.tsx
@@ -5,7 +5,7 @@ import { Story, Meta } from "@storybook/react/types-6-0"
 import FormSignUp from "."
 
 export default {
-  title: "FormSignUp",
+  title: "Form/FormSignUp",
   component: FormSignUp
 } as Meta
 
