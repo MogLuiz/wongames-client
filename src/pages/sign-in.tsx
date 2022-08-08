21e55633767849781732f990c3c213f77c4293a5
Luiz Henrique <luizhjramos@outlook.com>
2022-08-07 21:24:49 -0300

    feat(pages): add form component in the pages

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,9 +1,16 @@
 // Packages
 import React from "react"
 
+// Components
+import FormSignIn from "components/FormSignIn"
+
 // Template
 import Auth from "templates/Auth"
 
-const SignIn = () => <Auth title="Entrar">Entrar</Auth>
+const SignIn = () => (
+  <Auth title="Entrar">
+    <FormSignIn />
+  </Auth>
+)
 
 export default SignIn
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,9 +1,16 @@
 // Packages
 import React from "react"
 
+// Components
+import FormSignUp from "components/FormSignUp"
+
 // Template
 import Auth from "templates/Auth"
 
-const SignUp = () => <Auth title="Criar conta">Criar conta</Auth>
+const SignUp = () => (
+  <Auth title="Criar conta">
+    <FormSignUp />
+  </Auth>
+)
 
 export default SignUp
