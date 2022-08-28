3729ff88fa20f48b4d10a012206344e07f02d633
Luiz Henrique <luizhjramos@outlook.com>
2022-08-28 17:05:27 -0300

    feat: initialize test, style and component of the base template

---
+++ src/templates/Base/test.tsx
@@ -0,0 +1,15 @@
+// Packages
+import { render, screen } from "@testing-library/react"
+
+// Components
+import Base from "."
+
+describe("<Base />", () => {
+  it("should render the heading", () => {
+    const { container } = render(<Base />)
+
+    expect(screen.getByRole("heading", { name: /Base/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
