57c6d37793862ad3ba8b30e2ace737c0f52ef89c
Luiz Henrique <luizhjramos@outlook.com>
2022-09-02 20:46:02 -0300

    docs: GameDetails storybook component documentation

--- src/components/GameDetails/stories.tsx
+++ src/components/GameDetails/stories.tsx
@@ -5,8 +5,17 @@ import { Story, Meta } from "@storybook/react/types-6-0"
 import GameDetails from "."
 
 export default {
-  title: "GameDetails",
-  component: GameDetails
+  title: "Game/GameDetails",
+  component: GameDetails,
+  parameters: {
+    backgrounds: {
+      default: "won-dark"
+    }
+  }
 } as Meta
 
-export const Default: Story = () => <GameDetails />
+export const Default: Story = () => (
+  <div style={{ maxWidth: "130rem", margin: "0 auto" }}>
+    <GameDetails />
+  </div>
+)
