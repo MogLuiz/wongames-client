ee0cf50a5178925669ee8cda0d7d24cfe248f6e7
Luiz Henrique <luizhjramos@outlook.com>
2022-09-01 21:24:55 -0300

    test: ensure render buttons

--- src/components/GameInfo/test.tsx
+++ src/components/GameInfo/test.tsx
@@ -8,7 +8,7 @@ import GameInfo from "."
 const props = {
   title: "My Game Title",
   description: "Game Description",
-  price: "210.00"
+  price: "210,00"
 }
 
 describe("<GameInfo />", () => {
@@ -18,14 +18,18 @@ describe("<GameInfo />", () => {
     expect(
       screen.getByRole("heading", { name: /my game title/i })
     ).toBeInTheDocument()
-    expect(screen.getByText(/\$210,00/)).toBeInTheDocument()
+    expect(screen.getByText("$210,00")).toBeInTheDocument()
     expect(screen.getByText(/game description/i)).toBeInTheDocument()
   })
 
   it("should render buttons", () => {
     renderWithTheme(<GameInfo {...props} />)
 
-    // esperar button add to cart
-    // esperar button wishlist
+    expect(
+      screen.getByRole("button", { name: /add to cart/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole("button", { name: /wishlist/i })
+    ).toBeInTheDocument()
   })
 })
