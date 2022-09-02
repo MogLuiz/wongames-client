e4255a60e0b639c6e9c0b845919682e762de9632
Luiz Henrique <luizhjramos@outlook.com>
2022-09-02 20:14:01 -0300

    feat: GameDetails component structure

--- src/components/GameDetails/index.tsx
+++ src/components/GameDetails/index.tsx
@@ -1,12 +1,51 @@
 // Packages
 import React from "react"
 
+// Components
+import Heading from "components/Heading"
+import MediaMatch from "components/MediaMatch"
+
 // Styles
 import * as S from "./styles"
 
 const GameDetails = () => (
   <S.Wrapper>
-    <h1>GameDetails</h1>
+    <MediaMatch greaterThan="small">
+      <Heading lineLeft lineColor="secondary">
+        Game Details
+      </Heading>
+    </MediaMatch>
+
+    <S.Content>
+      <S.Block>
+        <S.Label>Developer</S.Label>
+        <S.Description>Gearbox Software</S.Description>
+      </S.Block>
+
+      <S.Block>
+        <S.Label>Release Date</S.Label>
+        <S.Description>Nov 16, 2019</S.Description>
+      </S.Block>
+
+      <S.Block>
+        <S.Label>Platforms</S.Label>
+      </S.Block>
+
+      <S.Block>
+        <S.Label>Publisher</S.Label>
+        <S.Description>2K</S.Description>
+      </S.Block>
+
+      <S.Block>
+        <S.Label>Rating</S.Label>
+        <S.Description>18+</S.Description>
+      </S.Block>
+
+      <S.Block>
+        <S.Label>Genres</S.Label>
+        <S.Description>Action / Adventure</S.Description>
+      </S.Block>
+    </S.Content>
   </S.Wrapper>
 )
 
