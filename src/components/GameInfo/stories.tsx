67f34fc02358defc75df8195d513030fc76b6093
Luiz Henrique <luizhjramos@outlook.com>
2022-09-01 22:17:50 -0300

    docs: GameInfo storybook component documentation

--- src/components/GameInfo/stories.tsx
+++ src/components/GameInfo/stories.tsx
@@ -1,12 +1,25 @@
 // Packages
 import { Story, Meta } from "@storybook/react/types-6-0"
 
+// Mock
+import mockGame from "./mock"
+
 // Components
-import GameInfo from "."
+import GameInfo, { GameInfoProps } from "."
 
 export default {
-  title: "GameInfo",
-  component: GameInfo
+  title: "Game/GameInfo",
+  component: GameInfo,
+  parameters: {
+    backgrounds: {
+      default: "won-dark"
+    }
+  },
+  args: mockGame
 } as Meta
 
-export const Default: Story = () => <GameInfo />
+export const Default: Story<GameInfoProps> = (args) => (
+  <div style={{ maxWidth: "144rem", margin: "auto", padding: "1.5rem" }}>
+    <GameInfo {...args} />
+  </div>
+)
