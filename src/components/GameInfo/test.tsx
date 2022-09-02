a9e54774e4d7f2ea6b44eb153d7e2c5dfc624b91
Luiz Henrique <luizhjramos@outlook.com>
2022-09-01 21:18:56 -0300

    test: ensure render game informations

--- src/components/GameInfo/test.tsx
+++ src/components/GameInfo/test.tsx
@@ -1,15 +1,31 @@
 // Packages
-import { render, screen } from "@testing-library/react"
+import { screen } from "@testing-library/react"
+import { renderWithTheme } from "utils/tests/helpers"
 
 // Components
 import GameInfo from "."
 
+const props = {
+  title: "My Game Title",
+  description: "Game Description",
+  price: "210.00"
+}
+
 describe("<GameInfo />", () => {
-  it("should render the heading", () => {
-    const { container } = render(<GameInfo />)
+  it("should render game informations", () => {
+    renderWithTheme(<GameInfo {...props} />)
 
-    expect(screen.getByRole("heading", { name: /GameInfo/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole("heading", { name: /my game title/i })
+    ).toBeInTheDocument()
+    expect(screen.getByText(/\$210,00/)).toBeInTheDocument()
+    expect(screen.getByText(/game description/i)).toBeInTheDocument()
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it("should render buttons", () => {
+    renderWithTheme(<GameInfo {...props} />)
+
+    // esperar button add to cart
+    // esperar button wishlist
   })
 })
