749069bbd6bd7152b39717ef5816fe74870a60c6
Luiz Henrique <luizhjramos@outlook.com>
2022-09-07 19:58:29 -0300

    test: ensure render with correct colors in different viewports

--- src/components/TextContent/test.tsx
+++ src/components/TextContent/test.tsx
@@ -28,4 +28,20 @@ describe("<TextContent />", () => {
       screen.queryByRole("heading", { name: /description/i })
     ).not.toBeInTheDocument()
   })
+
+  it("should render with correct colors in different viewports", () => {
+    renderWithTheme(<TextContent {...props} />)
+
+    const wrapper = screen.getByRole("heading", {
+      name: /description/i
+    }).parentElement
+
+    expect(wrapper).toHaveStyle({
+      color: "#FAFAFA" // theme.colors.white
+    })
+
+    expect(wrapper).toHaveStyleRule("color", "#030517", {
+      media: "(min-width: 768px)"
+    })
+  })
 })
