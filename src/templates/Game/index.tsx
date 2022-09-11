4ef894b15cc2a2454bda187363c852eaed2a491e
Luiz Henrique <luizhjramos@outlook.com>
2022-09-11 19:30:33 -0300

    feat: initialize Game template

---
+++ src/templates/Game/index.tsx
@@ -0,0 +1,13 @@
+// Packages
+import React from "react"
+
+// Styles
+import * as S from "./styles"
+
+const Game = () => (
+  <S.Wrapper>
+    <h1>Game</h1>
+  </S.Wrapper>
+)
+
+export default Game
