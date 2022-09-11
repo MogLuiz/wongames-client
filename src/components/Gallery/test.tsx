425bc3a99ab487a01614c448e36ab0b6bbecfac0
Luiz Henrique <luizhjramos@outlook.com>
2022-09-10 21:52:12 -0300

    test: ensure render thumbnails as buttons

--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -1,15 +1,21 @@
-// Packages
-import { render, screen } from "@testing-library/react"
+import "match-media-mock"
+import { fireEvent, screen } from "@testing-library/react"
+import { renderWithTheme } from "utils/tests/helpers"
 
-// Components
 import Gallery from "."
 
+import mockItems from "./mock"
+
 describe("<Gallery />", () => {
-  it("should render the heading", () => {
-    const { container } = render(<Gallery />)
+  it("should render thumbnails as buttons", () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
 
-    expect(screen.getByRole("heading", { name: /Gallery/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole("button", { name: /Thumb - Gallery Image 1/i })
+    ).toHaveAttribute("src", mockItems[0].src)
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole("button", { name: /Thumb - Gallery Image 2/i })
+    ).toHaveAttribute("src", mockItems[1].src)
   })
 })
