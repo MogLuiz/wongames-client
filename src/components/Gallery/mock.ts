04031690e06c156bea209e5290a28f319cfd1fcf
Luiz Henrique <luizhjramos@outlook.com>
2022-09-10 21:33:15 -0300

    docs: Gallery storybook documentation

--- src/components/Gallery/mock.ts
+++ src/components/Gallery/mock.ts
@@ -1,26 +1,26 @@
 export default [
   {
-    src: "/img/games/cyberpunk-1.jpg",
+    src: "/img/games/cyberpunk-1.jpeg",
     label: "Gallery Image 1"
   },
   {
-    src: "/img/games/cyberpunk-2.jpg",
+    src: "/img/games/cyberpunk-2.jpeg",
     label: "Gallery Image 2"
   },
   {
-    src: "/img/games/cyberpunk-3.jpg",
+    src: "/img/games/cyberpunk-3.jpeg",
     label: "Gallery Image 3"
   },
   {
-    src: "/img/games/cyberpunk-4.jpg",
+    src: "/img/games/cyberpunk-4.jpeg",
     label: "Gallery Image 4"
   },
   {
-    src: "/img/games/cyberpunk-5.jpg",
+    src: "/img/games/cyberpunk-5.jpeg",
     label: "Gallery Image 5"
   },
   {
-    src: "/img/games/cyberpunk-6.jpg",
+    src: "/img/games/cyberpunk-6.jpeg",
     label: "Gallery Image 6"
   }
 ]
--- src/components/Gallery/stories.tsx
+++ src/components/Gallery/stories.tsx
@@ -1,12 +1,23 @@
-// Packages
 import { Story, Meta } from "@storybook/react/types-6-0"
-
-// Components
-import Gallery from "."
+import Gallery, { GalleryProps } from "."
+import items from "./mock"
 
 export default {
   title: "Gallery",
-  component: Gallery
+  component: Gallery,
+  args: {
+    items
+  },
+  parameters: {
+    layout: "fullscreen",
+    backgrounds: {
+      default: "won-dark"
+    }
+  }
 } as Meta
 
-export const Default: Story = () => <Gallery />
+export const Default: Story<GalleryProps> = (args) => (
+  <div style={{ maxWidth: "130rem", margin: "0 auto" }}>
+    <Gallery {...args} />
+  </div>
+)
