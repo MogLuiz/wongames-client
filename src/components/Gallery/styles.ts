22ab39ecd8f548c3ec06cb1458fef76b30a1f4de
Luiz Henrique <luizhjramos@outlook.com>
2022-09-10 22:36:13 -0300

    styles: image overlay

--- src/components/Gallery/styles.ts
+++ src/components/Gallery/styles.ts
@@ -54,7 +54,19 @@ const modalModifiers = {
 }
 
 export const Modal = styled.div<ModalProps>`
-  ${({ isOpen }) => css`
+  ${({ isOpen, theme }) => css`
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.7);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: ${theme.layers.modal};
+    transition: opacity ${theme.transition.default};
+
     ${isOpen && modalModifiers.open()}
     ${!isOpen && modalModifiers.close()}
   `}
