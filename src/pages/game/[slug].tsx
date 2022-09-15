abcd817a009cf653cc343a48c6261e6f44cf0ec9
Luiz Henrique <luizhjramos@outlook.com>
2022-09-15 18:34:17 -0300

    feat: gallery page data

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -1,3 +1,5 @@
+import galleryMock from "components/Gallery/mock"
+
 import Game, { GameTemplateProps } from "templates/Game"
 
 const GamePage = (props: GameTemplateProps) => <Game {...props} />
@@ -19,7 +21,8 @@ export async function getStaticProps() {
         price: "59.00",
         description:
           "Cyberpunk 2077 is an open-world, action-adventure story set in Night City, a megalopolis obsessed with power, glamour and body modification. You play as V, a mercenary outlaw going after a one-of-a-kind implant that is the key to immortality"
-      }
+      },
+      gallery: galleryMock
     }
   }
 }
