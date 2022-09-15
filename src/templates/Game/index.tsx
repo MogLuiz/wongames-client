400cd5ff10a62a40fb106945b8529d0fb5fba08d
Luiz Henrique <luizhjramos@outlook.com>
2022-09-15 19:41:06 -0300

    feat: apply Showcase component in the Game template

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -4,6 +4,9 @@ import GameInfo, { GameInfoProps } from "components/GameInfo"
 import Gallery, { GalleryImageProps } from "components/Gallery"
 import TextContent from "components/TextContent"
 import GameDetails, { GameDetailsProps } from "components/GameDetails"
+import { GameCardProps } from "components/GameCard"
+import { HighlightProps } from "components/Highlight"
+import Showcase from "components/Showcase"
 
 import * as S from "./styles"
 
@@ -13,6 +16,9 @@ export type GameTemplateProps = {
   gallery?: GalleryImageProps[]
   description: string
   details: GameDetailsProps
+  upcommingGames: GameCardProps[]
+  upcommingHighlight: HighlightProps
+  recommendedGames: GameCardProps[]
 }
 
 const Game = ({
@@ -20,7 +26,10 @@ const Game = ({
   gameInfo,
   gallery,
   description,
-  details
+  details,
+  upcommingGames,
+  upcommingHighlight,
+  recommendedGames
 }: GameTemplateProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
@@ -41,6 +50,14 @@ const Game = ({
       <S.SectionGameDetails>
         <GameDetails {...details} />
       </S.SectionGameDetails>
+
+      <Showcase
+        title="Upcomming"
+        games={upcommingGames}
+        highlight={upcommingHighlight}
+      />
+
+      <Showcase title="You may like these games" games={recommendedGames} />
     </S.Main>
   </Base>
 )
