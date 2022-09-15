58551d90cf2939e5f01768b03938bf50c5bfa150
Luiz Henrique <luizhjramos@outlook.com>
2022-09-14 21:28:50 -0300

    feat: apply GameInfo component in the Game Template

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,14 +1,19 @@
 import Base from "templates/Base"
 
+import GameInfo, { GameInfoProps } from "components/GameInfo"
+
 import * as S from "./styles"
 
 export type GameTemplateProps = {
   cover: string
+  gameInfo: GameInfoProps
 }
 
-const Game = ({ cover }: GameTemplateProps) => (
+const Game = ({ cover, gameInfo }: GameTemplateProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
+
+    <GameInfo {...gameInfo} />
   </Base>
 )
 
