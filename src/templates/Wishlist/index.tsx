3792a45ab072ccd6cf28634a4195419e1e445cd0
Luiz Henrique <luizhjramos@outlook.com>
2022-09-24 19:29:11 -0300

    feat: initialize Wishlist template

---
+++ src/templates/Wishlist/index.tsx
@@ -0,0 +1,13 @@
+// Packages
+import React from "react"
+
+// Styles
+import * as S from "./styles"
+
+const Wishlist = () => (
+  <S.Wrapper>
+    <h1>Wishlist</h1>
+  </S.Wrapper>
+)
+
+export default Wishlist
---
+++ src/templates/Wishlist/test.tsx
@@ -0,0 +1,15 @@
+// Packages
+import { render, screen } from "@testing-library/react"
+
+// Components
+import Wishlist from "."
+
+describe("<Wishlist />", () => {
+  it("should render the heading", () => {
+    const { container } = render(<Wishlist />)
+
+    expect(screen.getByRole("heading", { name: /Wishlist/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
