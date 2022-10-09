6196c95850ba9444a2ca6bf28a4e73002072ba68
Luiz Henrique <luizhjramos@outlook.com>
2022-10-09 18:31:46 -0300

    feat: PaymentOptions JSX component

--- src/components/PaymentOptions/styles.ts
+++ src/components/PaymentOptions/styles.ts
@@ -1,4 +1,15 @@
-// Packages
 import styled from "styled-components"
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.div``
+
+export const Body = styled.div``
+
+export const Footer = styled.div``
+
+export const CardsList = styled.div``
+
+export const CardItem = styled.div``
+
+export const CardInfo = styled.div``
+
+export const AddCard = styled.div``
