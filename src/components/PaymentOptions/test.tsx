15a92ea92caf4d63e092a94d2db3308bc38d2a49
Luiz Henrique <luizhjramos@outlook.com>
2022-10-09 18:50:14 -0300

    test: ensure render the saved card options and the add new card button

--- src/components/PaymentOptions/test.tsx
+++ src/components/PaymentOptions/test.tsx
@@ -1,15 +1,15 @@
-// Packages
-import { render, screen } from "@testing-library/react"
+import { screen } from "@testing-library/react"
+import { renderWithTheme } from "utils/tests/helpers"
 
-// Components
 import PaymentOptions from "."
+import cards from "./mock"
 
 describe("<PaymentOptions />", () => {
-  it("should render the heading", () => {
-    const { container } = render(<PaymentOptions />)
+  it("should render the saved card options and the add new card button", () => {
+    renderWithTheme(<PaymentOptions cards={cards} handlePayment={jest.fn} />)
 
-    expect(screen.getByRole("heading", { name: /PaymentOptions/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByLabelText(/4325/)).toBeInTheDocument()
+    expect(screen.getByLabelText(/4326/)).toBeInTheDocument()
+    expect(screen.getByText(/add a new credit card/i)).toBeInTheDocument()
   })
 })
