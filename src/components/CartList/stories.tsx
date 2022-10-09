943461079f399dd85300c3b10a2b1171cea2c32f
Luiz Henrique <luizhjramos@outlook.com>
2022-10-09 13:07:11 -0300

    docs: CartList storybook documentation

--- src/components/CartList/stories.tsx
+++ src/components/CartList/stories.tsx
@@ -1,12 +1,29 @@
-// Packages
 import { Story, Meta } from "@storybook/react/types-6-0"
+import CartList, { CartListProps } from "."
 
-// Components
-import CartList from "."
+import mockItems from "./mock"
 
 export default {
   title: "CartList",
-  component: CartList
+  component: CartList,
+  args: {
+    items: mockItems,
+    total: "R$ 330,00"
+  },
+  argTypes: {
+    items: {
+      type: ""
+    }
+  },
+  parameters: {
+    backgrounds: {
+      default: "won-dark"
+    }
+  }
 } as Meta
 
-export const Default: Story = () => <CartList />
+export const Default: Story<CartListProps> = (args) => (
+  <div style={{ maxWidth: 800 }}>
+    <CartList {...args} />
+  </div>
+)
