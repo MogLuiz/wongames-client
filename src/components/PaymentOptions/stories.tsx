309859d55ab35eb3db3782da62965b7534747ba0
Luiz Henrique <luizhjramos@outlook.com>
2022-10-09 18:42:41 -0300

    docs: PaymentOptions storybook documentation

--- src/components/PaymentOptions/stories.tsx
+++ src/components/PaymentOptions/stories.tsx
@@ -1,12 +1,16 @@
-// Packages
 import { Story, Meta } from "@storybook/react/types-6-0"
+import PaymentOptions, { PaymentOptionsProps } from "."
 
-// Components
-import PaymentOptions from "."
+import cardsMock from "./mock"
 
 export default {
   title: "PaymentOptions",
-  component: PaymentOptions
+  component: PaymentOptions,
+  args: {
+    cards: cardsMock
+  }
 } as Meta
 
-export const Default: Story = () => <PaymentOptions />
+export const Default: Story<PaymentOptionsProps> = (args) => (
+  <PaymentOptions {...args} />
+)
