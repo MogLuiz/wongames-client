a0f223cfd088f1f7643d45ae2270269fc94efdc9
Luiz Henrique <luizhjramos@outlook.com>
2022-10-11 19:52:30 -0300

    feat: Cart template contract

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -1,8 +1,22 @@
-// Packages
-import React from "react"
+import Base from "templates/Base"
 
-// Styles
+import { Container } from "components/Container"
+import { Divider } from "components/Divider"
+import { GameCardProps } from "components/GameCard"
+import { HighlightProps } from "components/Highlight"
+import PaymentOptions, { PaymentOptionsProps } from "components/PaymentOptions"
+import CartList, { CartListProps } from "components/CartList"
+import Heading from "components/Heading"
+import Showcase from "components/Showcase"
+import Empty from "components/Empty"
+
 import * as S from "./styles"
+
+export type CartProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+} & CartListProps &
+  Pick<PaymentOptionsProps, "cards">
 
 const Cart = () => (
   <S.Wrapper>
