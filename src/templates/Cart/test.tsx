5ab0c6bdf9d7f7c5aa3d472ccc166d195022618c
Luiz Henrique <luizhjramos@outlook.com>
2022-10-11 20:14:53 -0300

    test: components Cart template jest mock

--- src/templates/Cart/test.tsx
+++ src/templates/Cart/test.tsx
@@ -1,13 +1,53 @@
-// Packages
-import { render, screen } from "@testing-library/react"
+import "match-media-mock"
+import { screen } from "@testing-library/react"
+import { renderWithTheme } from "utils/tests/helpers"
 
-// Components
+import gamesMock from "components/GameCardSlider/mock"
+import highlightMock from "components/Highlight/mock"
+import itemsMock from "components/CartList/mock"
+import cardsMock from "components/PaymentOptions/mock"
+
 import Cart from "."
 
-describe("<Cart />", () => {
-  // it("should render the heading", () => {
-  //   const { container } = render(<Cart />)
-  //   expect(screen.getByRole("heading", { name: /Cart/i })).toBeInTheDocument()
-  //   expect(container.firstChild).toMatchSnapshot()
-  // })
-})
+const props = {
+  items: itemsMock,
+  total: "$ 430,00",
+  cards: cardsMock,
+  recommendedHighlight: highlightMock,
+  recommendedGames: gamesMock
+}
+
+jest.mock("templates/Base", () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock Base">{children}</div>
+  }
+}))
+
+jest.mock("components/Showcase", () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock Showcase" />
+  }
+}))
+
+jest.mock("components/CartList", () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock Cart" />
+  }
+}))
+
+jest.mock("components/PaymentOptions", () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock PaymentOptions" />
+  }
+}))
+
+jest.mock("components/Empty", () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock Empty" />
+  }
+}))
