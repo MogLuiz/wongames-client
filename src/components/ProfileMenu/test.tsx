9ca558046798569cbe1c77522865f5abcecf7fd4
Luiz Henrique <luizhjramos@outlook.com>
2022-10-17 19:09:17 -0300

    test: ensure render the menu with an active link defined

--- src/components/ProfileMenu/test.tsx
+++ src/components/ProfileMenu/test.tsx
@@ -1,5 +1,6 @@
 import { screen } from "@testing-library/react"
 import { renderWithTheme } from "utils/tests/helpers"
+import theme from "styles/theme"
 
 import ProfileMenu from "."
 
@@ -16,5 +17,14 @@ describe("<ProfileMenu />", () => {
     expect(screen.getByRole("link", { name: /sign out/i })).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it("should render the menu with an active link defined", () => {
+    renderWithTheme(<ProfileMenu activeLink="/profile/cards" />)
+
+    expect(screen.getByRole("link", { name: /my cards/i })).toHaveStyle({
+      background: theme.colors.primary,
+      color: theme.colors.white
+    })
   })
 })
