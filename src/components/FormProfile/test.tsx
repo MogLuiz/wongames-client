a4619dafab29879bdee3e324120b758264111034
Luiz Henrique <luizhjramos@outlook.com>
2022-10-18 20:39:11 -0300

    test: render the profile form

--- src/components/FormProfile/test.tsx
+++ src/components/FormProfile/test.tsx
@@ -1,15 +1,25 @@
-// Packages
-import { render, screen } from "@testing-library/react"
+import { screen } from "@testing-library/react"
+import { renderWithTheme } from "utils/tests/helpers"
 
-// Components
 import FormProfile from "."
 
 describe("<FormProfile />", () => {
-  it("should render the heading", () => {
-    const { container } = render(<FormProfile />)
+  it("should render the profile form", () => {
+    renderWithTheme(<FormProfile />)
 
-    expect(screen.getByRole("heading", { name: /FormProfile/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole("heading", { name: /my profile/i })
+    ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByRole("textbox", { name: /name/i })).toBeInTheDocument()
+    expect(screen.getByRole("textbox", { name: /e-mail/i })).toBeInTheDocument()
+
+    expect(
+      screen.getByPlaceholderText(/type your password/i)
+    ).toBeInTheDocument()
+
+    expect(screen.getByPlaceholderText(/new password/i)).toBeInTheDocument()
+
+    expect(screen.getByRole("button", { name: /save/i })).toBeInTheDocument()
   })
 })
