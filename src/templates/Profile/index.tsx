e44496a33022c9b4ff74bf054e916b68e2517912
Luiz Henrique <luizhjramos@outlook.com>
2022-10-19 19:54:39 -0300

    feat: initialize Profile template

---
+++ src/templates/Profile/index.tsx
@@ -0,0 +1,13 @@
+// Packages
+import React from "react"
+
+// Styles
+import * as S from "./styles"
+
+const Profile = () => (
+  <S.Wrapper>
+    <h1>Profile</h1>
+  </S.Wrapper>
+)
+
+export default Profile
---
+++ src/templates/Profile/test.tsx
@@ -0,0 +1,15 @@
+// Packages
+import { render, screen } from "@testing-library/react"
+
+// Components
+import Profile from "."
+
+describe("<Profile />", () => {
+  it("should render the heading", () => {
+    const { container } = render(<Profile />)
+
+    expect(screen.getByRole("heading", { name: /Profile/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
