8010ea261f7bb13a0123f4f34b9e9c78c2842103
Luiz Henrique <luizhjramos@outlook.com>
2022-10-19 18:39:53 -0300

    test: ensure render the cards list

--- src/components/CardsList/test.tsx
+++ src/components/CardsList/test.tsx
@@ -1,15 +1,29 @@
-// Packages
-import { render, screen } from "@testing-library/react"
+import { screen } from "@testing-library/react"
 
-// Components
+import { renderWithTheme } from "utils/tests/helpers"
+
+import cardsMock from "components/PaymentOptions/mock"
 import CardsList from "."
 
 describe("<CardsList />", () => {
-  it("should render the heading", () => {
-    const { container } = render(<CardsList />)
+  it("should render the cards list", () => {
+    renderWithTheme(<CardsList cards={cardsMock} />)
 
-    expect(screen.getByRole("heading", { name: /CardsList/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole("heading", { name: /my cards/i })
+    ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByRole("img", { name: /visa/i })).toHaveAttribute(
+      "src",
+      "/img/visa.png"
+    )
+
+    expect(screen.getByRole("img", { name: /mastercard/i })).toHaveAttribute(
+      "src",
+      "/img/master-card.png"
+    )
+
+    expect(screen.getByText(/4325/)).toBeInTheDocument()
+    expect(screen.getByText(/4326/)).toBeInTheDocument()
   })
 })
