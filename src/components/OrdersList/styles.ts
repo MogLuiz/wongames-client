7b882bf8d16cb50bbc314a74414df44b64ba6bf7
Luiz Henrique <luizhjramos@outlook.com>
2022-10-20 19:27:29 -0300

    styles: OrdersList component

--- src/components/OrdersList/styles.ts
+++ src/components/OrdersList/styles.ts
@@ -1,4 +1,10 @@
-// Packages
 import styled from "styled-components"
+import * as GameItemStyles from "components/GameItem/styles"
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.div`
+  ${GameItemStyles.Wrapper} {
+    &:last-child {
+      border-bottom: 0;
+    }
+  }
+`
