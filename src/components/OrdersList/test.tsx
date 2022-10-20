7a593be88aca2695c3d9628111d296b3cbcec851
Luiz Henrique <luizhjramos@outlook.com>
2022-10-20 19:29:33 -0300

    test: ensure render the game items

--- src/components/OrdersList/test.tsx
+++ src/components/OrdersList/test.tsx
@@ -1,15 +1,31 @@
-// Packages
-import { render, screen } from "@testing-library/react"
+import { screen } from "@testing-library/react"
+import { renderWithTheme } from "utils/tests/helpers"
 
-// Components
 import OrdersList from "."
+import mock from "./mock"
+
+jest.mock("components/Empty", () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock Empty" />
+  }
+}))
+
+jest.mock("components/GameItem", () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock GameItem">{children}</div>
+  }
+}))
 
 describe("<OrdersList />", () => {
-  it("should render the heading", () => {
-    const { container } = render(<OrdersList />)
+  it("should render the game items", () => {
+    renderWithTheme(<OrdersList items={mock} />)
 
-    expect(screen.getByRole("heading", { name: /OrdersList/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole("heading", { name: /my orders/i })
+    ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getAllByTestId("Mock GameItem")).toHaveLength(2)
   })
 })
