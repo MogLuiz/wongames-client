e1cedb59348f4d6950f7f223249f4bf8af4073c4
Luiz Henrique <luizhjramos@outlook.com>
2022-10-23 16:30:14 -0300

    test: ensure render headings

--- src/components/ExploreSidebar/test.tsx
+++ src/components/ExploreSidebar/test.tsx
@@ -1,15 +1,19 @@
-// Packages
-import { render, screen } from "@testing-library/react"
+import { screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { renderWithTheme } from "utils/tests/helpers"
+import { css } from "styled-components"
 
-// Components
 import ExploreSidebar from "."
 
 describe("<ExploreSidebar />", () => {
-  it("should render the heading", () => {
-    const { container } = render(<ExploreSidebar />)
+  it("should render headings", () => {
+    renderWithTheme(<ExploreSidebar items={items} onFilter={jest.fn} />)
 
-    expect(screen.getByRole("heading", { name: /ExploreSidebar/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByRole("heading", { name: /price/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole("heading", { name: /sort by/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole("heading", { name: /system/i })).toBeInTheDocument()
+    expect(screen.getByRole("heading", { name: /genre/i })).toBeInTheDocument()
   })
 })
