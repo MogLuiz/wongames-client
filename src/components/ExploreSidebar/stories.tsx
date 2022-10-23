5ad95a01d8065e4b83ba12894a361be6399d94b4
Luiz Henrique <luizhjramos@outlook.com>
2022-10-23 17:31:48 -0300

    docs: ExploreSidebar storybook documentation

--- src/components/ExploreSidebar/stories.tsx
+++ src/components/ExploreSidebar/stories.tsx
@@ -1,12 +1,33 @@
-// Packages
 import { Story, Meta } from "@storybook/react/types-6-0"
+import ExploreSidebar, { ExploreSidebarProps } from "."
 
-// Components
-import ExploreSidebar from "."
+import items from "./mock"
 
 export default {
   title: "ExploreSidebar",
-  component: ExploreSidebar
+  component: ExploreSidebar,
+  args: {
+    items
+  },
+  parameters: {
+    layout: "fullscreen",
+    backgrounds: {
+      default: "won-dark"
+    }
+  }
 } as Meta
 
-export const Default: Story = () => <ExploreSidebar />
+export const Default: Story<ExploreSidebarProps> = (args) => (
+  <div style={{ padding: 16, maxWidth: 320 }}>
+    <ExploreSidebar {...args} />
+  </div>
+)
+
+export const WithInitialValues: Story<ExploreSidebarProps> = (args) => (
+  <div style={{ padding: 16, maxWidth: 320 }}>
+    <ExploreSidebar
+      {...args}
+      initialValues={{ windows: true, sort_by: "low-to-high" }}
+    />
+  </div>
+)
