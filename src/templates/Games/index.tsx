e16f82e08cbe8ac9435a1d54e5dbab880eda9f89
Luiz Henrique <luizhjramos@outlook.com>
2022-10-30 18:19:18 -0300

    feat: Games template

---
+++ src/templates/Games/index.tsx
@@ -0,0 +1,48 @@
+import Base from "templates/Base"
+import { KeyboardArrowDown as ArrowDown } from "@styled-icons/material-outlined/KeyboardArrowDown"
+
+import ExploreSidebar from "components/ExploreSidebar"
+import GameCard, { GameCardProps } from "components/GameCard"
+import { Grid } from "components/Grid"
+
+import { ItemProps } from "components/ExploreSidebar/types"
+
+import * as S from "./styles"
+
+export type GamesTemplateProps = {
+  games?: GameCardProps[]
+  filterItems: ItemProps[]
+}
+
+const GamesTemplate = ({ filterItems, games = [] }: GamesTemplateProps) => {
+  const handleFilter = () => {
+    return
+  }
+
+  const handleShowMore = () => {
+    return
+  }
+
+  return (
+    <Base>
+      <S.Main>
+        <ExploreSidebar items={filterItems} onFilter={handleFilter} />
+
+        <section>
+          <Grid>
+            {games.map((item) => (
+              <GameCard key={item.title} {...item} />
+            ))}
+          </Grid>
+
+          <S.ShowMore role="button" onClick={handleShowMore}>
+            <p>Show More</p>
+            <ArrowDown size={35} />
+          </S.ShowMore>
+        </section>
+      </S.Main>
+    </Base>
+  )
+}
+
+export default GamesTemplate
