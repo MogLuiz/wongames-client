d77f01b7f05950f1fd0e51aeddabf9ffc12f9956
Luiz Henrique <luizhjramos@outlook.com>
2022-11-01 20:44:05 -0300

    feat: initialize test, style, docs and JSX of the CartIcon component

---
+++ src/components/CartIcon/index.tsx
@@ -0,0 +1,13 @@
+// Packages
+import React from "react"
+
+// Styles
+import * as S from "./styles"
+
+const CartIcon = () => (
+  <S.Wrapper>
+    <h1>CartIcon</h1>
+  </S.Wrapper>
+)
+
+export default CartIcon
---
+++ src/components/CartIcon/stories.tsx
@@ -0,0 +1,12 @@
+// Packages
+import { Story, Meta } from "@storybook/react/types-6-0"
+
+// Components
+import CartIcon from "."
+
+export default {
+  title: "CartIcon",
+  component: CartIcon
+} as Meta
+
+export const Default: Story = () => <CartIcon />
