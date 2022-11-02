5ff81254901febb7beab3b43ce23a1d1ee3bd60c
Luiz Henrique <luizhjramos@outlook.com>
2022-11-02 18:01:13 -0300

    docs: Cartdropdown storybook documentation

--- src/components/CartDropdown/stories.tsx
+++ src/components/CartDropdown/stories.tsx
@@ -1,12 +1,24 @@
-// Packages
 import { Story, Meta } from "@storybook/react/types-6-0"
+import CartDropdown, { CartDropdownProps } from "."
 
-// Components
-import CartDropdown from "."
+import items from "components/CartList/mock"
 
 export default {
   title: "CartDropdown",
-  component: CartDropdown
+  component: CartDropdown,
+  args: {
+    items,
+    total: "R$ 300,00"
+  },
+  parameters: {
+    backgrounds: {
+      default: "won-dark"
+    }
+  }
 } as Meta
 
-export const Default: Story = () => <CartDropdown />
+export const Default: Story<CartDropdownProps> = (args) => (
+  <div style={{ maxWidth: "98%", display: "flex", justifyContent: "flex-end" }}>
+    <CartDropdown {...args} />
+  </div>
+)
