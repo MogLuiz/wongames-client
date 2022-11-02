0b786eea749ef4f5daf6262c78d26fb0b8c2b1ae
Luiz Henrique <luizhjramos@outlook.com>
2022-11-02 15:07:11 -0300

    feat: logic to open and close the Dropdown component

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -7,10 +7,15 @@ export type DropdownProps = {
   children: React.ReactNode
 }
 
-const Dropdown = ({ title, children }: DropdownProps) => (
-  <S.Wrapper>
-    <S.Title>{title}</S.Title>
-  </S.Wrapper>
-)
+const Dropdown = ({ title, children }: DropdownProps) => {
+  const [isOpen, setIsOpen] = useState(false)
 
+  return (
+    <S.Wrapper isOpen={isOpen}>
+      <S.Title onClick={() => setIsOpen(!isOpen)}>{title}</S.Title>
+
+      <S.Content aria-hidden={!isOpen}>{children}</S.Content>
+    </S.Wrapper>
+  )
+}
 export default Dropdown
