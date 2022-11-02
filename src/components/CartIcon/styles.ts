371502432d7f5da680a80f144cd2b41216e6fb1d
Luiz Henrique <luizhjramos@outlook.com>
2022-11-02 13:47:51 -0300

    styles: CartIcon component

--- src/components/CartIcon/styles.ts
+++ src/components/CartIcon/styles.ts
@@ -1,4 +1,27 @@
-// Packages
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    width: 2.4rem;
+    height: 2.4rem;
+    position: relative;
+  `}
+`
+
+export const Badge = styled.span`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: ${theme.colors.secondary};
+    color: ${theme.colors.white};
+    font-size: 1rem;
+    border-radius: 50%;
+    width: 1.6rem;
+    height: 1.6rem;
+    position: absolute;
+    top: -0.4rem;
+    right: -0.4rem;
+  `}
+`
