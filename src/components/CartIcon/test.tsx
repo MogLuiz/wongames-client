cf44e49d412094470c2fc7c449c83dcb83fb636f
Luiz Henrique <luizhjramos@outlook.com>
2022-11-02 13:47:03 -0300

    test: ensure render without badge

--- src/components/CartIcon/test.tsx
+++ src/components/CartIcon/test.tsx
@@ -1,15 +1,14 @@
-// Packages
-import { render, screen } from "@testing-library/react"
+import { screen } from "@testing-library/react"
 
-// Components
+import { renderWithTheme } from "utils/tests/helpers"
+
 import CartIcon from "."
 
 describe("<CartIcon />", () => {
-  it("should render the heading", () => {
-    const { container } = render(<CartIcon />)
+  it("should render without badge", () => {
+    renderWithTheme(<CartIcon />)
 
-    expect(screen.getByRole("heading", { name: /CartIcon/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
+    expect(screen.queryByLabelText(/cart items/i)).not.toBeInTheDocument()
   })
 })
