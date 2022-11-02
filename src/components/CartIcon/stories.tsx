e6e6a05a404aec538c520ffb8c264b573d830ee2
Luiz Henrique <luizhjramos@outlook.com>
2022-11-02 13:49:40 -0300

    docs: CartIcon storybook documentation

--- src/components/CartIcon/stories.tsx
+++ src/components/CartIcon/stories.tsx
@@ -1,12 +1,19 @@
-// Packages
 import { Story, Meta } from "@storybook/react/types-6-0"
-
-// Components
-import CartIcon from "."
+import CartIcon, { CartIconProps } from "."
 
 export default {
   title: "CartIcon",
-  component: CartIcon
+  component: CartIcon,
+  parameters: {
+    backgrounds: {
+      default: "won-dark"
+    }
+  }
 } as Meta
 
 export const Default: Story = () => <CartIcon />
+export const withItems: Story<CartIconProps> = (args) => <CartIcon {...args} />
+
+withItems.args = {
+  quantity: 3
+}
