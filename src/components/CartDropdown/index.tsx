17df89102a222cdfb78b1ffb9a9e00250cc50452
Luiz Henrique <luizhjramos@outlook.com>
2022-11-02 17:56:54 -0300

    feat: CartDropdown JSX structure

--- src/components/CartDropdown/index.tsx
+++ src/components/CartDropdown/index.tsx
@@ -1,12 +1,20 @@
-// Packages
-import React from "react"
+import CartIcon from "components/CartIcon"
+import CartList from "components/CartList"
+import Dropdown from "components/Dropdown"
+import { GameItemProps } from "components/GameItem"
 
-// Styles
 import * as S from "./styles"
 
-const CartDropdown = () => (
+export type CartDropdownProps = {
+  items: GameItemProps[]
+  total: string
+}
+
+const CartDropdown = ({ items, total }: CartDropdownProps) => (
   <S.Wrapper>
-    <h1>CartDropdown</h1>
+    <Dropdown title={<CartIcon quantity={items.length} />}>
+      <CartList items={items} total={total} hasButton />
+    </Dropdown>
   </S.Wrapper>
 )
 
