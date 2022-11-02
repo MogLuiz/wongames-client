4be4e5e2b6c7bf3f2bbd96e1186d64b46f153b36
Luiz Henrique <luizhjramos@outlook.com>
2022-11-02 13:51:30 -0300

    test: ensure render with badge

--- src/components/CartIcon/test.tsx
+++ src/components/CartIcon/test.tsx
@@ -11,4 +11,11 @@ describe("<CartIcon />", () => {
     expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
     expect(screen.queryByLabelText(/cart items/i)).not.toBeInTheDocument()
   })
+
+  it("should render with badge", () => {
+    renderWithTheme(<CartIcon quantity={3} />)
+
+    expect(screen.getByLabelText(/cart items/i)).toBeInTheDocument()
+    expect(screen.getByText(/3/)).toBeInTheDocument()
+  })
 })
