1ce79098f183139c73c021b6cdff82fdba6080b9
Luiz Henrique <luizhjramos@outlook.com>
2022-11-08 21:57:38 -0300

    docs: UserDropdown storybook documentation

--- src/components/UserDropdown/stories.tsx
+++ src/components/UserDropdown/stories.tsx
@@ -1,12 +1,22 @@
-// Packages
 import { Story, Meta } from "@storybook/react/types-6-0"
-
-// Components
-import UserDropdown from "."
+import UserDropdown, { UserDropdownProps } from "."
 
 export default {
   title: "UserDropdown",
-  component: UserDropdown
+  component: UserDropdown,
+  parameters: {
+    backgrounds: {
+      default: "won-dark"
+    }
+  }
 } as Meta
 
-export const Default: Story = () => <UserDropdown />
+export const Default: Story<UserDropdownProps> = (args) => (
+  <div style={{ maxWidth: "98%", display: "flex", justifyContent: "flex-end" }}>
+    <UserDropdown {...args} />
+  </div>
+)
+
+Default.args = {
+  username: "Willian"
+}
