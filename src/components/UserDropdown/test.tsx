8391935742e0bb75e9082ddfe51794c87234f6df
Luiz Henrique <luizhjramos@outlook.com>
2022-11-08 22:08:13 -0300

    test: ensure render the menu

--- src/components/UserDropdown/test.tsx
+++ src/components/UserDropdown/test.tsx
@@ -10,4 +10,17 @@ describe("<UserDropdown />", () => {
 
     expect(screen.getByText(/willian/i)).toBeInTheDocument()
   })
+
+  it("should render the menu", () => {
+    renderWithTheme(<UserDropdown username="Willian" />)
+
+    userEvent.click(screen.getByText(/willian/i))
+
+    expect(
+      screen.getByRole("link", { name: /my profile/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole("link", { name: /wishlist/i })).toBeInTheDocument()
+    expect(screen.getByRole("link", { name: /sign out/i })).toBeInTheDocument()
+  })
 })
