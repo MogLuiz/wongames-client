712550bdb0898064523184b8c82651fd85a9a1c7
Luiz Henrique <luizhjramos@outlook.com>
2022-11-16 21:19:19 -0300

    feat: apply apollo client graphql client in the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,35 @@
-// import App from "next/app";
 import type { AppProps } from "next/app"
 import Head from "next/head"
 
-// Styles
 import { ThemeProvider } from "styled-components"
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
+
 import theme from "styles/theme"
 import GlobalStyles from "styles/global"
 
 function App({ Component, pageProps }: AppProps) {
+  const client = new ApolloClient({
+    uri: "http://localhost:1337/graphql",
+    cache: new InMemoryCache()
+  })
+
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won Games</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="description" content="The best Game Stores in the world!" />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Won Games</title>
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="description"
+            content="The best Game Stores in the world!"
+          />
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 
