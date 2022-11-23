53b847e5e8f71bfa4202c02710be6a06d0dc3e1d
Luiz Henrique <luizhjramos@outlook.com>
2022-11-23 06:26:23 -0300

    feat: QueryGameBySlug apollo client query

---
+++ src/graphql/queries/game.ts
@@ -0,0 +1,33 @@
+import { gql } from "@apollo/client"
+
+export const QUERY_GAME_BY_SLUG = gql`
+  query QueryGameBySlug($slug: String!) {
+    games(where: { slug: $slug }) {
+      name
+      short_description
+      description
+      price
+      rating
+      release_date
+      gallery {
+        src: url
+        label: alternativeText
+      }
+      cover {
+        src: url
+      }
+      developers {
+        name
+      }
+      publisher {
+        name
+      }
+      categories {
+        name
+      }
+      platforms {
+        name
+      }
+    }
+  }
+`
