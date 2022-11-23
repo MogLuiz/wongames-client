dc0de359b79142a02c3f5553258d16048634ad88
Luiz Henrique <luizhjramos@outlook.com>
2022-11-22 21:36:15 -0300

    feat: populate games page with the api data

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,16 +1,46 @@
 import GamesTemplate, { GamesTemplateProps } from "templates/Games"
 
+import { gql } from "@apollo/client"
+import { initializeApollo } from "services/apollo"
+
 import filterItemsMock from "components/ExploreSidebar/mock"
-import gamesMock from "components/GameCardSlider/mock"
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query({
+    query: gql`
+      query QueryGames {
+        games {
+          name
+          slug
+          cover {
+            url
+          }
+          developers {
+            name
+          }
+          price
+        }
+      }
+    `
+  })
   return {
     props: {
-      games: gamesMock,
+      revalidate: 60,
+      games: data?.games?.map((game) => ({
+        title: game?.name,
+        developer: game.developers[0]?.name || "",
+        img: `http://localhost:1337${game?.cover?.url}`,
+        price: new Intl.NumberFormat("en", {
+          style: "currency",
+          currency: "USD"
+        }).format(game?.price)
+      })),
       filterItems: filterItemsMock
     }
   }
