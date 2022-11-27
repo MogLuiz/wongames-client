1c293ae11aaddee380d6e3d66e68994932db882e
Luiz Henrique <luizhjramos@outlook.com>
2022-11-27 18:46:06 -0300

    feat: banners data fetch

--- src/graphql/generated/QueryHome.ts
+++ src/graphql/generated/QueryHome.ts
@@ -3,39 +3,42 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { ENUM_COMPONENTPAGERIBBON_COLOR, ENUM_COMPONENTPAGERIBBON_SIZE } from "./globalTypes";
+import {
+  ENUM_COMPONENTPAGERIBBON_COLOR,
+  ENUM_COMPONENTPAGERIBBON_SIZE
+} from "./globalTypes"
 
 // ====================================================
 // GraphQL query operation: QueryHome
 // ====================================================
 
 export interface QueryHome_banners_image {
-  __typename: "UploadFile";
-  url: string;
+  __typename: "UploadFile"
+  url: string
 }
 
 export interface QueryHome_banners_button {
-  __typename: "ComponentPageButton";
-  label: string;
-  link: string;
+  __typename: "ComponentPageButton"
+  label: string
+  link: string
 }
 
 export interface QueryHome_banners_ribbon {
-  __typename: "ComponentPageRibbon";
-  text: string | null;
-  color: ENUM_COMPONENTPAGERIBBON_COLOR | null;
-  size: ENUM_COMPONENTPAGERIBBON_SIZE | null;
+  __typename: "ComponentPageRibbon"
+  text: string | null
+  color: ENUM_COMPONENTPAGERIBBON_COLOR | null
+  size: ENUM_COMPONENTPAGERIBBON_SIZE | null
 }
 
 export interface QueryHome_banners {
-  __typename: "Banner";
-  image: QueryHome_banners_image | null;
-  title: string;
-  subtitle: string;
-  button: QueryHome_banners_button | null;
-  ribbon: QueryHome_banners_ribbon | null;
+  __typename: "Banner"
+  image: QueryHome_banners_image | null
+  title: string
+  subtitle: string
+  button: QueryHome_banners_button | null
+  ribbon: QueryHome_banners_ribbon | null
 }
 
 export interface QueryHome {
-  banners: QueryHome_banners[];
+  banners: QueryHome_banners[]
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,45 @@
-// Template
 import Home, { HomeTemplateProps } from "templates/Home"
 
-// Components
-import bannersMock from "components/BannerSlider/mock"
-import gamesMock from "components/GameCardSlider/mock"
+import { QUERY_HOME } from "graphql/queries/home"
+import { initializeApollo } from "services/apollo"
+import { QueryHome, QueryHome_banners } from "graphql/generated/QueryHome"
+
 import highlightMock from "components/Highlight/mock"
+import gamesMock from "components/GameCardSlider/mock"
+
+const bannerDataFactory = ({
+  button,
+  image,
+  ribbon,
+  subtitle,
+  title
+}: QueryHome_banners) => ({
+  img: `http://localhost:1337${image?.url}`,
+  title: title,
+  subtitle: subtitle,
+  buttonLabel: button?.label,
+  buttonLink: button?.link,
+  ...(ribbon && {
+    ribbon: ribbon.text,
+    ribbonColor: ribbon.color,
+    ribbonSize: ribbon.size
+  })
+})
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
 export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+
+  const bannersFactoryComposition = data?.banners.map(bannerDataFactory)
+
   return {
     props: {
-      banners: bannersMock,
+      revalidate: 10,
+      banners: bannersFactoryComposition,
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
