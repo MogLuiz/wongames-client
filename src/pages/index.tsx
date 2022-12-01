d9c2bb65ba0e09a5da754d0e32f1bb03adbe16c4
Luiz Henrique <luizhjramos@outlook.com>
2022-12-01 20:09:09 -0300

    feat: new games factory data composition

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,11 @@ import Home, { HomeTemplateProps } from "templates/Home"
 
 import { QUERY_HOME } from "graphql/queries/home"
 import { initializeApollo } from "services/apollo"
-import { QueryHome, QueryHome_banners } from "graphql/generated/QueryHome"
+import {
+  QueryHome,
+  QueryHome_banners,
+  QueryHome_newGames
+} from "graphql/generated/QueryHome"
 
 import highlightMock from "components/Highlight/mock"
 import gamesMock from "components/GameCardSlider/mock"
@@ -26,21 +30,38 @@ const bannerDataFactory = ({
   })
 })
 
+const newGamesFactory = ({
+  name,
+  slug,
+  developers,
+  cover,
+  price
+}: QueryHome_newGames) => ({
+  title: name,
+  slug: slug,
+  developer: developers[0].name,
+  img: `http://localhost:1337${cover?.url}`,
+  price: price
+})
+
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
-  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+  const {
+    data: { banners, newGames }
+  } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
 
-  const bannersFactoryComposition = data?.banners.map(bannerDataFactory)
+  const bannersFactoryComposition = banners.map(bannerDataFactory)
+  const newGamesFactoryComposition = newGames.map(newGamesFactory)
 
   return {
     props: {
       revalidate: 10,
       banners: bannersFactoryComposition,
-      newGames: gamesMock,
+      newGames: newGamesFactoryComposition,
       mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
       upcommingGames: gamesMock,
