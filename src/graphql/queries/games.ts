b06bd08f3994de09bd054e6e5053ca22fab0ff7d
Luiz Henrique <luizhjramos@outlook.com>
2022-12-01 19:55:25 -0300

    refactor: apply game fragment on the QUERY_GAMES to reduce code

--- src/graphql/queries/games.ts
+++ src/graphql/queries/games.ts
@@ -1,17 +1,11 @@
 import { gql } from "@apollo/client"
+import { GameFragment } from "graphql/fragments/game"
 
 export const QUERY_GAMES = gql`
   query QueryGames($limit: Int!) {
     games(limit: $limit) {
-      name
-      slug
-      cover {
-        url
-      }
-      developers {
-        name
-      }
-      price
+      ...GameFragment
     }
   }
+  ${GameFragment}
 `
